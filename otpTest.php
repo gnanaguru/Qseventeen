02fe3272c24caaf8f2421865cf06011c738bcf79
Gnanaguru Mariyadass <Gnanaguru.Mariyadass@qfor.com>
2017-04-04 20:11:04 +0530

    update otp ajax

--- otpTest.php
+++ otpTest.php
@@ -1,7 +1,37 @@
 <?php
-if($_POST['approach'] == 'check' && $_POST['otpvalue'] == '1234'){
-	echo "true";
-}else{
-	echo "fail";
+include_once('wp-load.php');
+if($_POST['approach'] == 'verify'){
+	$intPhoneNumber = trim($_POST['intphonenumber']);
+	$intOtpProvide = trim($_POST['otpvalue']);
+	global $wpdb;
+	$isrecordAvailable = $wpdb->get_row( "SELECT * FROM wp_otp WHERE otp = $intOtpProvide and phonenumber = $intPhoneNumber and status ='started' ", ARRAY_A );
+	//print_r($mylink);
+
+	if ( null !== $isrecordAvailable ) {
+		$wpdb->update(  'wp_otp',  array(  'status' => 'verified' ), array( 'id' => $isrecordAvailable['id'] ),  array(  '%s' ), array( '%d' ) );
+	 	echo "verified.";
+	} else {
+	 	echo "wrong otp";
+	}
+}else if($_POST['approach'] == 'generate'){
+	global $wpdb;
+	$intPhoneNumber = trim($_POST['intphonenumber']);
+	$intRandotp = rand('1000','9999');
+	$isrecordAvailable = $wpdb->get_row( "SELECT * FROM wp_otp WHERE phonenumber = $intPhoneNumber and status ='started' ", ARRAY_A );
+	if ( null !== $isrecordAvailable ) {
+	 	echo "in";
+	 	$wpdb->update(  'wp_otp',  array(  'otp' => $intRandotp ), array( 'id' => $isrecordAvailable['id'] ),  array(  '%s' ), array( '%d' ) );
+	 	echo $wpdb->show_errors();
+	 	echo "otp updated.";
+	} else {
+	 	$wpdb->insert('wp_otp', array(  'otp' => $intRandotp,  'phonenumber' => $intPhoneNumber, 'status'=> 'started' ), array(  '%s', '%s', '%s' )  );
+		echo "true";
+	}
+
+	
+}else {
+	echo "not allowed";
 }
+
+
 ?>
--- wp-content/themes/wordpress/template-parts/content-page-otp.php
+++ wp-content/themes/wordpress/template-parts/content-page-otp.php
@@ -1,66 +1,99 @@
  <?php
-  print_r($_POST['otpValue']);
-if(isset($_POST['Validate']) && $_POST['Validate'] != ''){
-  print_r($_POST['otpValue']);
-  exit();
-
-}
-
 ?>
 <script type="text/javascript">
   jQuery(document).ready(function(){
     jQuery('#otpValidate').click(function(e){
-      e.preventDefault();
-      var tvalue = document.getElementById("otpValue").value;
-      alert(tvalue);
+      var tvalue = document.getElementById("otpValue").value;  
+      var intPhonenumber = document.getElementById('frmphone').value;
+      if(tvalue == ''){
+        alert('please enter otp');
+        return false
+      }
+
+      //verify otp
       jQuery.ajax({
           method: 'POST',
-          url: 'http://wordpress461.com/otpTest.php',
-          data:{ 'approach':'check', 'otpvalue': tvalue} 
+          url: 'http://l.w.qseventeen.com/otpTest.php',
+          async:false,
+          data:{ 'approach':'verify', 'otpvalue': tvalue, 'intphonenumber': intPhonenumber} 
       })
       .done(function(data){
-        if(data == 'true'){
-          alert('success')
-          var form = jQuery(this);
-          var inputs = form.find("input, select, button, textarea");
-          var serializedData = form.serialize();
-          alert(serializedData);
-          jQuery.post({
-            method: 'POST',
-            url: 'http://wordpress461.com/otpTest.php',
-            data:{ 'approach':'submit', 'postvalues':serializedData}
-          }).done(function(data){
-            alert(data);
-            alert('done-post values');
-          }).fail(function(){
-            alert(data);
-            alert('fail-post values');
-          });
-          jQuery('#review-form').submit();
-          return true;
-        }else if(data != 'true'){
+        if(data == 'verified'){
+          alert('verified');
+        }else if(data == 'wrong otp'){
           alert('wrong, please entre valid number');
-          return false;
+          e.preventDefault();
         }
       })
       .fail(function(event){
-        alert('failure');
-        return false;
+        alert('failure call');
+      });
+
+
+
+      //e.preventDefault();
+    });
+
+    jQuery('#frmsend').click(function(){
+      if(document.getElementById('frmfirstname').value == ''){
+        alert('please enter your firstname');
+        return false
+      }
+
+      if(document.getElementById('frmlastname').value == ''){
+        alert('please enter your lastname');
+        return false
+      }
+
+      if(document.getElementById('frmemail').value == ''){
+        alert('please enter your email');
+        return false
+      }
+
+      if(document.getElementById('frmphone').value == ''){
+        alert('please enter your mobile number');
+        return false
+      }
+
+
+      var intPhonenumber = document.getElementById('frmphone').value;
+      //generate otp
+      jQuery.ajax({
+          method: 'POST',
+          url: 'http://l.w.qseventeen.com/otpTest.php',
+          data:{ 'approach':'generate', 'intphonenumber': intPhonenumber} 
+      }).done(function(data){
+          jQuery('#otparea').slideDown(400);
+      }).fail(function(data){
+          alert('failure call');
       });
 
     });
   });
+
 </script>
+<style type="text/css">
+  .otparea{ display: none; }
+</style>
 
-<?php 
-  echo $intRandNumber = rand(1000,9999);
+<?php
+
+if(isset($_POST['Validate']) && $_POST['Validate'] == 'Verify'){
+  print_r($_POST);
+}
+
 ?>
-
-
- <div class="content-area" style="width: 50%; margin: 0 auto;">
-  <h1 class="page-title">Q Verify</h1>
-  <form id="otpverifyform" method="post" action="#" >
-  <input type="text" id="otpValue" value="1234" placeholder="enter otp">
-  <input type="submit" id="otpValidate" name="Validate" value="Validate">
+<div class="content-area" style="width: 50%; margin: 0 auto;">
+<h1 class="page-title">Q Form</h1>
+  <form id="otpverifyform" method="post" action="">
+    <div class="otparea" id="otparea">
+      <input type="text" id="otpValue" value="" placeholder="enter otp">
+      <input type="submit" id="otpValidate" name="Validate" value="Verify">
+    </div>
+    <input type="text" id="frmfirstname" name="frmfirstname" value="gnana" placeholder="firstname">
+    <input type="text" id="frmlastname" name="frmlastname" value="guru" placeholder="lastname">
+    <input type="email" id="frmemail" name="frmemail" value="gnanangurumari@gmail.com" placeholder="gnanangurumari@gmail.com">
+    <input type="text" id="frmphone" name="frmphone" value="9962468686" placeholder="99624268686">
+    <input type="button" id="frmsend" name="frmsend" value="Send">
   </form>
 </div> 
